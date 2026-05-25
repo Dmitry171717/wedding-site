8e9b32ee66159a870cc9e7e7e5d6cb5c40e12fb9
Dmitry171717 <84243643+Dmitry171717@users.noreply.github.com>
2026-05-25 22:42:57 +0500

    Update send-form.js

--- netlify/functions/send-form.js
+++ netlify/functions/send-form.js
@@ -1,46 +1,45 @@
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzACM0al5yKqzIlXs1wwwNC5VecYCb2irtEzUVP5YDOMo_lce4myeklTV-09-7QtRjV/exec';
+const GOOGLE_SCRIPT_URL =
+'https://script.google.com/macros/s/AKfycbzACM0al5yKqzIlXs1wwwNC5VecYCb2irtEzUVP5YDOMo_lce4myeklTV-09-7QtRjV/exec';
 
 exports.handler = async (event) => {
-  try {
-    const params = new URLSearchParams(event.body);
+try {
 
-    const name = params.get('name') || '';
-    const attendance = params.get('attendance') || '';
-    const comment = params.get('comment') || '';
+const data = JSON.parse(event.body);
 
-    const response = await fetch(GOOGLE_SCRIPT_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/plain;charset=utf-8'
-      },
-      body: JSON.stringify({
-        name,
-        attendance,
-        comment
-      })
-    });
+const response = await fetch(GOOGLE_SCRIPT_URL,{
+method:'POST',
+headers:{
+'Content-Type':'application/json'
+},
+body: JSON.stringify({
+name:data.name || '',
+attendance:data.attendance || '',
+comment:data.comment || ''
+})
+});
 
-    const text = await response.text();
-console.log('Google Script response:', text);
-
-if (!response.ok) {
-throw new Error('Google Script error: ' + text);
+if(!response.ok){
+throw new Error('Google Script error');
 }
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ success: true })
-    };
+return {
+statusCode:200,
+body:JSON.stringify({
+success:true
+})
+};
 
-  } catch (error) {
-    console.log('ОШИБКА:', error);
+}catch(error){
 
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        success: false,
-        error: error.toString()
-      })
-    };
-  }
+console.log(error);
+
+return{
+statusCode:500,
+body:JSON.stringify({
+success:false,
+error:error.toString()
+})
+};
+
+}
 };
